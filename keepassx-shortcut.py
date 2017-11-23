5b5bd98f9b28ffa1a3c7281760e3d4294eb2938e
Tamnil Saito Junior <tamnil@gmail.com>
2017-11-23 17:18:07 -0200

    ajustado folder

--- keepassx-shortcut.py
+++ keepassx-shortcut.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
-
 # click on keepassX indicaton via shortcut. insert in shortcut list
 
 import pyautogui
 
-keepassLocation = pyautogui.locateCenterOnScreen('keepassx.png')
+keepassLocation = pyautogui.locateCenterOnScreen('/opt/keepassx-shortcut/keepassx.png')
 pyautogui.click(keepassLocation)
 
